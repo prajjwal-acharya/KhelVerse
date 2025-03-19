5ddcb6500b47d9e77ee2020cd1f51ba64b0bdf73
Tanya Vardhan <tanyavardhan2005@gmail.com>
2025-03-19 08:09:11 +0530

    lp

--- src/components/landingPage/AboutUs.jsx
+++ src/components/landingPage/AboutUs.jsx
@@ -35,7 +35,7 @@ function AboutUs() {
 
       {/* Content Section */}
       <motion.div
-        className="bg-white h-[85vh] w-full flex flex-col-reverse md:flex-row items-center justify-around"
+        className="bg-white h-[85vh] w-full flex flex-col-reverse md:flex-row items-center justify-center"
         style={{
           opacity: 1 + scrollPercentage * 0, // Ranges from 0.4 to 1 (0% to 100% visibility)
           filter: `blur(${(1 - scrollPercentage) * 5}px)`, // More blur when less visible
@@ -48,18 +48,18 @@ function AboutUs() {
             Revolutionizing <span className="text-purple">Athlete Management</span> for{" "}
             <span className="text-purple">Peak Performance.</span>
           </h2>
-          <p className=" text-md sm:text-lg text-left max-w-[600px]">
+          <p className=" mb-4 text-md sm:text-lg text-left max-w-[600px]">
             Minimize setbacks, maximize potential, and build champions. A unified platform that streamlines training,
             tracks progress, and supports career growth—empowering athletes, coaches, and organizations to achieve
             excellence.
           </p>
         </div>
         {/* Right Content */}
-        <div className="w-[50vw] md:w-[40vw] h-[40vh] mr-10">
+        <div className="w-[50vw] md:w-[40vw]  h-[30vh] sm:h-[40vh] mr-10 flex items-center">
         <img 
             src="https://res.cloudinary.com/dgj1gzq0l/image/upload/v1742274119/atheletedash_pgsvuv.svg" 
             alt="laptop-logo" 
-            className="h-full w-auto"
+            className="h-auto w-auto"
           />
         </div>
       </motion.div>
